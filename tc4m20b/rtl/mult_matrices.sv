7a973477eeb4d9c9a93d7143d1c7b8d82bfb3262
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-03-18 12:21:25 -0300

    up

--- tc4m20b/rtl/mult_matrices.sv
+++ tc4m20b/rtl/mult_matrices.sv
@@ -1,37 +1,37 @@
 module MatrixDelta
    import packMatrix::*;
     (
-      input  param P,
-      output param soma
+      input  param16 P,
+      output param16 soma
     );
+    timeunit 1ns;
+    timeprecision 1ps;
 
-      param tc, tc;
-      // reg32 soma;
+    regC sp1, sp5, sp9, sp13;
 
-      always_comb begin
+    always_comb begin
 
-        // CSA_3 sc0 (P[0], P[1], P[2],  tc[0]);
         assign soma[0] =  - P[0] + P[2];
-        CSA_2 sc1 (P[1], P[2],  tc[1]);
-        assign soma[1] = tc[1];
+        CSA_2 csa_p1 (P[1], P[2],  sp1);
+        assign soma[1] = sp1;
         assign soma[2] =  - P[1] + P[2];
         assign soma[3] =  - P[1] + P[3];
 
         assign soma[4] =  - P[4] + P[6];
-        CSA_2 sc5 (P[5], P[6], tc[5]);
-        assign soma[5] = tc[5];
+        CSA_2 csa_p5 (P[5], P[6], sp5);
+        assign soma[5] = sp5;
         assign soma[6] =  - P[5] + P[6];
         assign soma[7] =  - P[5] + P[7];
 
         assign soma[8] =  + P[10] - P[8];
-        CSA_2 sc5 (P[10], P[9], tc[9]);
-        assign soma[9] =  tc[9];
+        CSA_2 csa_p9 (P[10], P[9], sp9);
+        assign soma[9] =  sp9;
         assign soma[10] =  P[10] - P[9];
         assign soma[11] =  P[11] - P[9];
 
         assign soma[12] =  - P[12] - P[14];
-        CSA_2 sc5 (P[13], P[14], tc[13]);
-        assign soma[13] =  tc[13];
+        CSA_2 csa_p13(P[13], P[14], sp13);
+        assign soma[13] =  sp13;
         assign soma[14] =  P[10] - P[9];
         assign soma[15] =  P[11] - P[9];
     end
@@ -41,11 +41,13 @@ endmodule
 module MatrixD
    import packMatrix::*;
     (
-      input  param P,
-      output param soma
+      input  param16 P,
+      output param16 soma
     );
+    timeunit 1ns;
+    timeprecision 1ps;
 
-      param tc, tc;
+      regC sp4, sp5, sp6, sp7;
       // reg32 soma;
 
       always_comb begin
@@ -55,14 +57,14 @@ module MatrixD
         assign soma[2] =  - P[2] + P[10];
         assign soma[3] =  - P[3] + P[11];
 
-        CSA_2 sc5 (P[4], P[8], tc[4]);
-        assign soma[4] = tc[4];
-        CSA_2 sc5 (P[5], P[9], tc[5]);
-        assign soma[5] = tc[5];
-        CSA_2 sc5 (P[6], P[10], tc[6]);
-        assign soma[6] = tc[6];
-        CSA_2 sc5 (P[7], P[11], tc[7]);
-        assign soma[7] = tc[7];
+        CSA_2 csa_p5 (P[4], P[8], sp4);
+        assign soma[4] = sp4;
+        CSA_2 csa_p5 (P[5], P[9], sp5);
+        assign soma[5] = sp5;
+        CSA_2 csa_p5 (P[6], P[10], sp6);
+        assign soma[6] = sp6;
+        CSA_2 csa_p5 (P[7], P[11], sp7);
+        assign soma[7] = sp7;
 
         assign soma[8] =  - P[4] + P[8];
         assign soma[9] =  - P[5] + P[9];
@@ -80,32 +82,32 @@ endmodule
 module MatrixSigma
    import packMatrix::*;
     (
-      input  param P,
-      output param soma
+      input  param16 P,
+      output param8 soma
     );
+    timeunit 1ns;
+    timeprecision 1ps;
 
-      param tc, tc;
-      // reg32 soma;
+    regC sp0, sp2, sp4, sp6, sp1, sp3, sp5, sp7;
 
-      always_comb begin
+    always_comb begin
+        CSA_3 csa_p0(P[0], P[1], P[2], sp0);
+        assign soma[0] = sp0;
+        CSA_3 csa_p2(P[4], P[5], P[6], sp2);
+        assign soma[2] = sp2;
+        CSA_3 csa_p4(P[8], P[9], P[10], sp4);
+        assign soma[4] = sp4;
+        CSA_3 csa_p6(P[12], P[13], P[14], sp6);
+        assign soma[6] = sp6;
 
-        CSA_3 sc0(P[0], P[1], P[2], tc[0]);
-        assign soma[0] = tc[0];
-        CSA_3 sc2(P[4], P[5], P[6], tc[2]);
-        assign soma[2] = tc[2];
-        CSA_3 sc4(P[8], P[9], P[10], tc[4]);
-        assign soma[4] = tc[4];
-        CSA_3 sc6(P[12], P[13], P[14], tc[6]);
-        assign soma[6] = tc[6];
-
-        CSA_2 sc1(P[1], P[3], tc[1]);
-        assign soma[1] = tc[1] - P[2];
-        CSA_2 sc3(P[5], P[7], tc[3]);
-        assign soma[3] = tc[3] - P[6];
-        CSA_2 sc5(P[9], P[11], tc[5]);
-        assign soma[5] = tc[5] - P[10];
-        CSA_2 sc7(P[13], P[15], tc[7]);
-        assign soma[5] = tc[7] - P[14];
+        CSA_2 csa_p1(P[1], P[3], sp1);
+        assign soma[1] = sp1 - P[2];
+        CSA_2 csa_p3(P[5], P[7], sp3);
+        assign soma[3] = sp3 - P[6];
+        CSA_2 csa_p5(P[9], P[11], sp5);
+        assign soma[5] = sp5 - P[10];
+        CSA_2 csa_p7(P[13], P[15], sp7);
+        assign soma[7] = sp7 - P[14];
     end
 endmodule
 
@@ -113,24 +115,25 @@ endmodule
 module MatrixS
    import packMatrix::*;
     (
-      input  param P,
-      output param soma
+      input  param8 P,
+      output param4 soma
     );
+    timeunit 1ns;
+    timeprecision 1ps;
 
-      param tc, tc;
-      // reg32 soma;
+    regC sp0, sp2, sp1, sp3;
 
-      always_comb begin
+    always_comb begin
 
-        CSA_3 sc0(P[0], P[2], P[4], tc[0]);
-        assign soma[0] = tc[0];
-        CSA_3 sc2(P[1], P[3], P[5], tc[2]);
-        assign soma[2] = tc[2];
+        CSA_3 csa_p0(P[0], P[2], P[4], sp0);
+        assign soma[0] = sp0;
+        CSA_3 csa_p2(P[1], P[3], P[5], sp2);
+        assign soma[2] = sp2;
 
-        CSA_2 sc1(P[2], P[6], tc[1]);
-        assign soma[1] = tc[1] - P[4];
-        CSA_2 sc3(P[3], P[7], tc[3]);
-        assign soma[3] = tc[3] - P[5];
+        CSA_2 csa_p1(P[2], P[6], sp1);
+        assign soma[1] = sp1 - P[4];
+        CSA_2 csa_p3(P[3], P[7], sp3);
+        assign soma[3] = sp3 - P[5];
     end
 endmodule
 
