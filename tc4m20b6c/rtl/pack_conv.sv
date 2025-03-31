44f2e15ed8f16b0e09238dc6c9967591456c47eb
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-03-31 02:01:23 -0300

    up

--- tc4m20b6c/rtl/pack_conv.sv
+++ tc4m20b6c/rtl/pack_conv.sv
@@ -3,30 +3,29 @@
 //-------------------------------------------------------------------------
 package packConv;
 
-
   timeunit 1ns;
   timeprecision 1ps;
   
 
-    parameter int NBITS = 20;   // 32 bits generate too large hardware!!
+  parameter int NBITS = 20;   // 32 bits generate too large hardware!!
 
-    typedef logic [NBITS-1:0] logic_vector;
+  typedef logic [NBITS-1:0] logic_vector;
 
-    // definitions for matrix multiplications
-    typedef logic_vector logic_vector16 [0:16];  // array with 16 parameters
-    typedef logic_vector logic_vector8  [0:8];   // array with  8 parameters
-    typedef logic_vector logic_vector4  [0:4];   // array with  4 parameters
-  
-    // definitions for the CSA adders
-    typedef logic_vector two_words    [1:0];
-    typedef logic_vector four_words   [3:0];
-    typedef logic_vector six_words    [5:0];
-    typedef logic_vector eight_words  [7:0];
-    typedef logic_vector ten_words    [9:0];
-  
-    // constants to control the multipliers
-    typedef enum  {
-        MU1, MU2, MU3, MU4
-    } mul_states;
+  // definitions for matrix multiplications
+  typedef logic_vector[16:0] logic_vector16;  // array with 16 parameters
+  typedef logic_vector[08:0] logic_vector8;     // array with  8 parameters
+  typedef logic_vector[04:0] logic_vector4;     // array with  4 parameters
+
+  // definitions for the CSA adders
+  typedef logic_vector[1:0] two_words   ;
+  typedef logic_vector[3:0] four_words  ;
+  typedef logic_vector[5:0] six_words   ;
+  typedef logic_vector[7:0] eight_words ;
+  typedef logic_vector[9:0] ten_words   ;
+
+  // constants to control the multipliers
+  typedef enum  {
+      MU1, MU2, MU3, MU4
+  } mul_states;
 
 endpackage
--- tc4m20b6c/tb_fast_conv.sv
+++ tc4m20b6c/tb_fast_conv.sv
@@ -3,103 +3,79 @@
 // -------------------------------------------------------------------------
 module tb;
 
-   timeunit 1ns;
-   timeprecision 1ps;
+  timeunit 1ns;
+  timeprecision 1ps;
 
-    import packConv::*;
+  import packConv::*;
+  import data::*;
 
-    logic_vector[0:16] weight, inputMAP;
-    logic_vector[0:4] outputMAP;
+  logic_vector16 weight, inputMAP;
+  logic_vector4 outputMAP;
 
-    logic reset, start, data_valid;
-    logic clk = 1'b0;
+  logic reset, start, data_valid;
+  logic clk = 1'b0;
 
-    // Quantized weights
-    // typedef int window_t[0:15];  // Define the 'window' type as an array of integers
 
-    // Constants for weights (normalized, multiplied by 256)
-    const int weights[] = '{
-      0, -384, -128, -512,
-      -1152, 2304, 768, 1920,
-      -384, 768, 256, 640,
-      -1536, 2688, 896, 2048
-    };
+  // Instantiate conv_rapida entity
+  conv_rapida #(
+    .QUANT(8)
+  ) convolucao (
+    .clk(clk),
+    .reset(reset),
+    .start(start),
+    .inputMAP(inputMAP),
+    .weights(weight),
+    .outputMAP(outputMAP),
+    .data_valid(data_valid)
+  );
 
-    // 3 input maps
-    // typedef window_t maps_array_t[ ];  // Define the 'maps_array' type
-    const int MAPS[][] = '{
-        '{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-    };
+  // Clock generation - 10 ns
+  always #5 clk = ~clk;
 
-    // Instantiate conv_rapida entity
-    conv_rapida #(
-        .QUANT(8)
-    ) convolucao (
-        .clk(clk),
-        .reset(reset),
-        .start(start),
-        .inputMAP(inputMAP),
-        .weights(weight),
-        .outputMAP(outputMAP),
-        .data_valid(data_valid)
-    );
+  // Test process to iterate over the input maps
+  initial begin
+    integer i, j, k;
 
-    // print the expected output
-    always @(posedge clk) begin
-        if (data_valid) begin
-            // Loop para imprimir os valores de outputMAP
-            $display("Time: %0t | Data Valid: %b", $time, data_valid);
-            $display("OutputMAP Values:");
-            for (int i = 0; i < 4; i = i + 1) begin
-                $display("outputMAP[%0d] = %d", i, ($signed(outputMAP[i])) );
-            end
-        end
+    // Configurações iniciais
+    $dumpfile("dump.vcd");  // Arquivo VCD para waveform
+    $dumpvars(0, tb);
+
+    // Monitor para debug
+    $monitor("Time: %0t | start: %b | data_valid: %b  j:%0d", $time, start, data_valid, j);
+
+    //clk = 0;
+    reset = 1;
+    #5 reset = 0;  // Liberar o reset após 5 ns
+
+    // Convert const_weight
+    for (i = 0; i < 16; i++) begin
+      assign weight[i] = (NBITS)'($signed(const_weight[i]));
     end
 
-
-    // Clock generation - 10 ns
-    always #5 clk = ~clk;
-
-    // Convert weights
-    genvar i;
-    generate
-        for (i = 0; i < 16; i++) begin
-            assign weight[i] = (NBITS)'($signed(weights[i]));
-        end
-    endgenerate;
-
-    // Test process to iterate over the input maps
-    initial begin
-        integer j, k;
-
-        // Configurações iniciais
-        $dumpfile("dump.vcd");  // Arquivo VCD para waveform
-        $dumpvars(0, tb);
-
-        // Monitor para debug
-        $monitor("Time: %0t | start: %b | data_valid: %b  j:%0d", $time, start, data_valid, j);
-
-        //clk = 0;
-        reset = 1;
-        #5 reset = 0;  // Liberar o reset após 5 ns
-
-        // Loop de simulação
-        for (j = 0; j <=  MAPS.size()-1; j++) begin
-            for (k = 0; k < 16; k++) begin
-                 inputMAP[k] = (NBITS)'($signed(MAPS[j][k]));
-            end
-
-            start = 1'b1;
-            #10 start = 1'b0;
-
-            wait(data_valid);
-
-            #100;  // Wait for 100 ns
+    // Loop de simulação
+    for (j = 0; j <=  const_feature_in.size()-1; j++) begin
+        for (k = 0; k < 16; k++) begin
+              inputMAP[k] = (NBITS)'($signed(const_feature_in[j][k]));
         end
 
-        // Finalizar a simulação 200 ns após o loop
-        #200 $finish;
+        start = 1'b1;
+        #10 start = 1'b0;
+
+        wait(data_valid);
+
+        // print the expected output
+        $display("Time: %0t | Data Valid: %b", $time, data_valid);
+        $display("OutputMAP Values:");
+        for (int k = 0; k < 4; k = k + 1) begin
+          $display("outputMAP[%0d] = %d", k, $signed(outputMAP[k]), $signed(const_feature_out[j][k]), "\t", $signed(outputMAP[k]) == $signed(const_feature_out[j][k][19:0]));
+        end
+
+        #100;  // Wait for 100 ns
     end
+
+    // Finalizar a simulação 200 ns após o loop
+    #200 $finish;
+  end
 
 
 endmodule
