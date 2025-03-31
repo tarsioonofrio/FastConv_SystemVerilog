fd7e58460e5c14f90e6c9c7d8cdaa27f2e2eb038
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-03-31 17:41:00 -0300

    up

--- tc4m20b6c/tb_fast_conv.sv
+++ tc4m20b6c/tb_fast_conv.sv
@@ -34,50 +34,50 @@ module tb;
 
   // Test process to iterate over the input maps
   initial begin
-    integer i, j, k;
 
     // Configurações iniciais
     $dumpfile("dump.vcd");  // Arquivo VCD para waveform
     $dumpvars(0, tb);
 
     // Monitor para debug
-    $monitor("Time: %0t | start: %b | data_valid: %b  j:%0d", $time, start, data_valid, j);
+    $monitor("Time: %0t | start: %b | data_valid: %b", $time, start, data_valid);
 
     //clk = 0;
     reset = 1;
     #5 reset = 0;  // Liberar o reset após 5 ns
 
     // Convert const_weight
-    for (i = 0; i < 16; i++) begin
-      assign weight[i] = (NBITS)'($signed(const_weight[0][i]));
-    end
+    for (int wi = 0; wi < const_weight.size(); wi++) begin
+      for (int wj = 0; wj < const_weight[0].size(); wj++) begin
+        assign weight[wj] = (NBITS)'($signed(const_weight[wi][wj]));
+      end
 
-    // Loop de simulação
-    for (j = 0; j < const_feature_in.size(); j++) begin
-        for (k = 0; k < const_feature_in[j].size(); k++) begin
-              inputMAP[k] = (NBITS)'($signed(const_feature_in[j][k]));
-        end
-
-        start = 1'b1;
-        #10 start = 1'b0;
-
-        wait(data_valid);
-
-        // print the expected output
-        $display("Time: %0t | Data Valid: %b", $time, data_valid);
-        for (int k = 0; k < 4; k = k + 1) begin
-          if ($signed(outputMAP[k]) != $signed(const_feature_out[j][k][19:0])) begin
-            $display("OutputMAP Values Error:");
-            $display("outputMAP[%0d] = %d", k, $signed(outputMAP[k]), $signed(const_feature_out[j][k]));
+      // Loop de simulação
+      for (int fi = 0; fi < const_feature_in.size(); fi++) begin
+          for (int fj = 0; fj < const_feature_in[fi].size(); fj++) begin
+                inputMAP[fj] = (NBITS)'($signed(const_feature_in[fi][fj]));
           end
-        end
-
-        #100;  // Wait for 100 ns
+  
+          start = 1'b1;
+          #10 start = 1'b0;
+  
+          wait(data_valid);
+  
+          // print the expected output
+          $display("Time: %0t | Data Valid: %b", $time, data_valid);
+          for (int fj = 0; fj < 4; fj = fj + 1) begin
+            if ($signed(outputMAP[fj]) != $signed(const_feature_out[fi][fj][19:0])) begin
+              $display("OutputMAP Values Error:");
+              $display("outputMAP[%0d] = %d", fj, $signed(outputMAP[fj]), $signed(const_feature_out[fi][fj]));
+            end
+          end
+  
+          #100;  // Wait for 100 ns
+      end
     end
 
     // Finalizar a simulação 200 ns após o loop
     #200 $finish;
   end
-
-
+  
 endmodule
