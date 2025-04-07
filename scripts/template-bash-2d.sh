500f4c195dba555e7e701e4725eec28fdff2df6d
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-04-06 21:53:38 -0300

    up

--- scripts/template-bash-2d.sh
+++ scripts/template-bash-2d.sh
@@ -1,13 +1,17 @@
-cd $1
+# $1=path/to/project 
+# $2=output_size
+# $3=bind
+# $4=shifts
 
+cd $1
 # init fast-conv repository with 2d convolution and output of size (3,3)
 fast-conv init 2d -o $2
 # build a fast conv 2d using toom cook method
 # fast-conv build 2d toom-cook
 # bind 2d fast convolution with nested method
-# fast-conv build 2d bind $3
+fast-conv build 2d bind $3
 # quantizate with 4 shifs
-# fast-conv quant shift -b $4
+fast-conv quant shift -b $4
 
 sim rand -s 032 -n 032 -d 032
 sim rand -s 064 -n 064 -d 064
