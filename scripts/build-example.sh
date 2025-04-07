8f1a8a7973172bd96b4e5140bf982c030f31f76b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-04-06 23:48:17 -0300

    multiple simulation for tc4n c8

--- scripts/build-example.sh
+++ scripts/build-example.sh
@@ -1,4 +1,4 @@
-# $1=path/to/project 
+# $1=path/to/project
 # $2=output_size
 # $3=bind
 # $4=shifts
@@ -6,15 +6,19 @@
 cd $1
 # init fast-conv repository with 2d convolution and output of size (3,3)
 fast-conv init 2d -o $2
+
 # build a fast conv 2d using toom cook method
 # fast-conv build 2d toom-cook
+
 # bind 2d fast convolution with nested method
 fast-conv build 2d bind $3
 # quantizate with 4 shifs
 fast-conv quant shift -b $4
 
-sim rand -s 032 -n 032 -d 032
-sim rand -s 064 -n 064 -d 064
-sim rand -s 128 -n 128 -d 128
-sim rand -s 256 -n 256 -d 256
-sim rand -s 512 -n 512 -d 512
+fast-conv sim rand -s 032 -n 032 -d 032
+fast-conv sim rand -s 064 -n 064 -d 064
+fast-conv sim rand -s 128 -n 128 -d 128
+fast-conv sim rand -s 256 -n 256 -d 256
+fast-conv sim rand -s 512 -n 512 -d 512
+
+rm -rf clib
---
+++ scripts/multiple-make.sh
@@ -0,0 +1,6 @@
+cd $1
+make DATA=data/sim/file-032/data.sv clean run
+make DATA=data/sim/file-064/data.sv clean run
+make DATA=data/sim/file-128/data.sv clean run
+make DATA=data/sim/file-256/data.sv clean run
+make DATA=data/sim/file-512/data.sv clean run
