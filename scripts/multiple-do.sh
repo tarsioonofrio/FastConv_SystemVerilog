2f846b0d3631b5e52f047d730da1be71ea8e3cfd
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-04-16 00:20:22 -0300

    up

--- scripts/multiple-do.sh
+++ scripts/multiple-do.sh
@@ -1,18 +1,18 @@
 cd $1
 DATA=data/sim/file-032
-DATA=$DATA/data.sv vsim -c -do sim.do
+DATA=$DATA/data.sv vsim -c -do sim.do > $DATA/run.txt
 cp sim_summary.txt $DATA/
 DATA=data/sim/file-064
-DATA=$DATA/data.sv vsim -c -do sim.do
+DATA=$DATA/data.sv vsim -c -do sim.do > $DATA/run.txt
 cp sim_summary.txt $DATA/
 DATA=data/sim/file-128
-DATA=$DATA/data.sv vsim -c -do sim.do
+DATA=$DATA/data.sv vsim -c -do sim.do > $DATA/run.txt
 cp sim_summary.txt $DATA/
 DATA=data/sim/file-256
-DATA=$DATA/data.sv vsim -c -do sim.do
+DATA=$DATA/data.sv vsim -c -do sim.do > $DATA/run.txt
 cp sim_summary.txt $DATA/
 DATA=data/sim/file-512
-DATA=$DATA/data.sv sim -c -do sim.do
+DATA=$DATA/data.sv sim -c -do sim.do > $DATA/run.txt
 cp sim_summary.txt $DATA/
 # to restore base sim_summary.txt
 vsim -c -do sim.do
