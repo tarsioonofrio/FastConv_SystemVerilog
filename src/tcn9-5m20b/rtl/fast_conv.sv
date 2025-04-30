3cbcab745782fead27571480da088df58d126c85
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-04-30 19:15:08 -0300

    tcn9 is working

--- src/tcn9-5m20b/rtl/fast_conv.sv
+++ src/tcn9-5m20b/rtl/fast_conv.sv
@@ -1,5 +1,5 @@
 //-------------------------------------------------------------------------
-// FERNANDO MORAES                                             October/2024
+// FERNANDO MORAES             October/2024
 //-------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------
@@ -8,144 +8,136 @@
 module conv
   import packConv::*;
  #(
-    parameter int QUANT = 8
+  parameter int QUANT = 8
   )
-  ( input  logic   clk, reset, start,
-    input  type_input inputMAP,
+  (
+    input  logic       clk, reset, start,
+    input  type_input  inputMAP,
     input  type_weight weights,
     output type_output outputMAP,
-    output logic   data_valid
+    output logic       data_valid
  );
 
-   timeunit 1ns;
-   timeprecision 1ps;
+  timeunit 1ns;
+  timeprecision 1ps;
 
 
-   type_input registers, prod_c0;
-   type_matrix_c prod_c1;
-   type_matrix_a prod_a1;
-   type_output prod_a0;
+  type_input registers, prod_c0;
+  type_matrix_c prod_c1;
+  type_matrix_a prod_a1;
+  type_output prod_a0;
 
-    logic signed [NBITS-1+QUANT:0] partial_product [0:4];   // QUANT more bits for the multipliers
+  logic signed [NBITS-1+QUANT:0] partial_product [0:4];   // QUANT more bits for the multipliers
 
-    logic [4:0] m0, m1, m2, m3, m4;
+  logic [4:0] m0, m1, m2, m3, m4;
 
-    typedef enum {IDLE, WR_IFMAP, WR_MC, MU1, MU2, MU3, MU4, MU5, WR_OUT} state_type;
+  typedef enum {IDLE, WR_IFMAP, WR_MC, MU1, MU2, MU3, MU4, MU5, WR_OUT} state_type;
 
-    state_type EA, PE;
+  state_type EA, PE;
 
-    //
-    // Control FSM
-    //
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
-            EA <= IDLE;
-        end else begin
-            EA <= PE;
-        end
+  //
+  // Control FSM
+  //
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      EA <= IDLE;
+    end else begin
+      EA <= PE;
     end
+  end
 
-    always_comb begin
-        unique case (EA)
-            IDLE:      PE = start ? WR_IFMAP : IDLE;
-            WR_IFMAP:  PE = WR_MC;
-            WR_MC:     PE = MU1;
+  always_comb begin
+    unique case (EA)
+      IDLE:     PE = start ? WR_IFMAP : IDLE;
+      WR_IFMAP: PE = WR_MC;
+      WR_MC:    PE = MU1;
 
-            // five state multiplier
-            MU1:     PE = MU2;
-            MU2:     PE = MU3;
-            MU3:     PE = MU4;
-            MU4:     PE = MU5;
-            MU5:     PE = WR_OUT;
+      // five state multiplier
+      MU1:   PE = MU2;
+      MU2:   PE = MU3;
+      MU3:   PE = MU4;
+      MU4:   PE = MU5;
+      MU5:   PE = WR_OUT;
 
-            //MMMA:      PE = WR_OUT;
-            WR_OUT:    PE = IDLE;
-        endcase
-    end
+      //MMMA:  PE = WR_OUT;
+      WR_OUT:  PE = IDLE;
+    endcase
+  end
 
-    //
-    // Data path
-    //
+  //
+  // Data path
+  //
 
-    // Instance of matrix multiplier "C"
-    MatrixC0 matrix_c0(
-      .P(registers),
-      .soma(prod_c0)
-    );
+  // Instance of matrix multiplier "C"
+  MatrixC0 matrix_c0(
+    .P(registers),
+    .soma(prod_c0)
+  );
 
-
-    MatrixC1 matrix_c1(
-      .P(prod_c0),
-      .soma(prod_c1)
-    );
+  MatrixC1 matrix_c1(
+    .P(prod_c0),
+    .soma(prod_c1)
+  );
 
    // 5 multipliers inside this block
-    always_comb begin
-
-          unique case (EA)
-                MU1: begin m0= 0; m1= 1; m2= 2;  m3= 3; m4= 4; end
-                MU2: begin m0= 5; m1= 6; m2= 7;  m3= 8; m4= 9; end
-                MU3: begin m0=10; m1=11; m2=12;  m3=13; m4=14; end
-                MU4: begin m0=15; m1=16; m2=17;  m3=18; m4=19; end
-                MU5: begin m0=20; m1=21; m2=22;  m3=23; m4=24; end
-         endcase
-
-          partial_product[0] = (NBITS+QUANT)'($signed(registers[m0]) * $signed(weights[m0]) );
-          partial_product[1] = (NBITS+QUANT)'($signed(registers[m1]) * $signed(weights[m1]) );
-          partial_product[2] = (NBITS+QUANT)'($signed(registers[m2]) * $signed(weights[m2]) );
-          partial_product[3] = (NBITS+QUANT)'($signed(registers[m3]) * $signed(weights[m3]) );
-          partial_product[4] = (NBITS+QUANT)'($signed(registers[m4]) * $signed(weights[m4]) );
-
-    end
+  always_comb begin
+    unique case (EA)
+      MU1: begin m0= 0; m1= 1; m2= 2;  m3= 3; m4= 4; end
+      MU2: begin m0= 5; m1= 6; m2= 7;  m3= 8; m4= 9; end
+      MU3: begin m0=10; m1=11; m2=12;  m3=13; m4=14; end
+      MU4: begin m0=15; m1=16; m2=17;  m3=18; m4=19; end
+      default: begin m0=20; m1=21; m2=22;  m3=23; m4=24; end
+    endcase
+    partial_product[0] = (NBITS+QUANT)'($signed(registers[m0]) * $signed(weights[m0]) );
+    partial_product[1] = (NBITS+QUANT)'($signed(registers[m1]) * $signed(weights[m1]) );
+    partial_product[2] = (NBITS+QUANT)'($signed(registers[m2]) * $signed(weights[m2]) );
+    partial_product[3] = (NBITS+QUANT)'($signed(registers[m3]) * $signed(weights[m3]) );
+    partial_product[4] = (NBITS+QUANT)'($signed(registers[m4]) * $signed(weights[m4]) );
+  end
 
 
-    // Instance of matrix multiplier "A"
-    MatrixA1 matrix_a1 (
-      .P(registers),
-      .soma(prod_a1)
-    );
+  // Instance of matrix multiplier "A"
+  MatrixA1 matrix_a1 (
+    .P(registers),
+    .soma(prod_a1)
+  );
 
-    MatrixA0 matrix_a0 (
-      .P(prod_a1),
-      .soma(prod_a0)
-    );
+  MatrixA0 matrix_a0 (
+    .P(prod_a1),
+    .soma(prod_a0)
+  );
 
-    // Internal register bank to store intermediate results
-    always_ff @(posedge clk or posedge reset) begin
+  // Internal register bank to store intermediate results
+  always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
-        registers <= '{default: '0};
-        //outputMAP <= '{default: '0};
-        data_valid <= 0;
+      registers <= '{default: '0};
+      //outputMAP <= '{default: '0};
+      data_valid <= 0;
     end
     else begin
-           data_valid <= 0;  // default
-               unique case (EA)
-                   WR_IFMAP:   registers <= inputMAP;
-
-                   WR_MC:      registers <= prod_c1;
-
-                   MU1, MU2, MU3, MU4, MU5:  begin
-                              registers[m0] <= (NBITS)'(partial_product[0][NBITS-1+QUANT:QUANT]);
-                              registers[m1] <= (NBITS)'(partial_product[1][NBITS-1+QUANT:QUANT]);
-                              registers[m2] <= (NBITS)'(partial_product[2][NBITS-1+QUANT:QUANT]);
-                              registers[m3] <= (NBITS)'(partial_product[3][NBITS-1+QUANT:QUANT]);
-                              registers[m4] <= (NBITS)'(partial_product[4][NBITS-1+QUANT:QUANT]);
-                        end
-
-                   WR_OUT: //begin
-                       data_valid <= 1;
-                    //  for (int i = 0; i < 9; i++)
-                    //      outputMAP[i] <= prod_a0[i];   /// saída registrada
-                    //  end
-               endcase
+      data_valid <= 0;  // default
+      unique case (EA)
+        WR_IFMAP:   registers <= inputMAP;
+        WR_MC:      registers <= prod_c1;
+        MU1, MU2, MU3, MU4, MU5:  begin
+          registers[m0] <= (NBITS)'(partial_product[0][NBITS-1+QUANT:QUANT]);
+          registers[m1] <= (NBITS)'(partial_product[1][NBITS-1+QUANT:QUANT]);
+          registers[m2] <= (NBITS)'(partial_product[2][NBITS-1+QUANT:QUANT]);
+          registers[m3] <= (NBITS)'(partial_product[3][NBITS-1+QUANT:QUANT]);
+          registers[m4] <= (NBITS)'(partial_product[4][NBITS-1+QUANT:QUANT]);
         end
-     end
-
-    always_latch begin
-      if (EA==WR_OUT) begin
-           for (int i = 0; i < 9; i++)
-                 outputMAP[i] = prod_a0[i];   /// saída em latch
-          end
+        WR_OUT: data_valid <= 1;
+        default: begin   // necessary - wrong behavior in logic simulation
+          registers <= registers;
+        end
+      endcase
     end
+  end
 
+  always_latch begin
+    if (EA==WR_OUT) begin
+      for (int i = 0; i < 9; i++)
+      outputMAP[i] = prod_a0[i];   /// saída em latch
+    end
+  end
 endmodule
