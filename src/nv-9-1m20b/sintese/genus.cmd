e566412ea92d70c2f8031b46b2b6ff695826ed1d
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-05-01 23:40:14 -0300

    up

--- src/nv-9-1m20b/sintese/genus.cmd
+++ src/nv-9-1m20b/sintese/genus.cmd
@@ -1,7 +1,7 @@
-# Cadence Genus(TM) Synthesis Solution, Version 22.15-s086_1, built Feb 28 2024 16:52:45
+# Cadence Genus(TM) Synthesis Solution, Version 22.17-s071_1, built Sep 12 2024 12:37:05
 
-# Date: Wed Oct 09 21:34:06 2024
-# Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.16.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
+# Date: Thu May 01 23:35:46 2025
+# Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.47.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
 # OS:   Rocky Linux release 8.10 (Green Obsidian)
 
 source run_logical_synthesis.tcl
