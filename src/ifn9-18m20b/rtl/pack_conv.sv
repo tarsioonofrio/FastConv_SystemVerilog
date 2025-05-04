aea31e47f9ae8f286fd1c9cf982a2dc7759aa304
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-05-04 00:05:17 -0300

    up

---
+++ src/ifn9-18m20b/rtl/pack_conv.sv
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------------------
+// FERNANDO MORAES                                          24/October/2024
+//-------------------------------------------------------------------------
+package packConv;
+  import data::*;
+
+  timeunit 1ns;
+  timeprecision 1ps;
+
+  parameter int NBITS = 20;
+
+  typedef logic [NBITS-1:0] logic_vector;
+
+  // definitions for matrix multiplications
+  typedef logic_vector type_weight   [35:0];
+  typedef logic_vector type_input    [24:0];
+  typedef logic_vector type_output   [8:0];
+  typedef logic_vector type_matrix_c [0:C1_SIZE*M1_SIZE];
+  typedef logic_vector type_matrix_a [0:A1_SIZE*M1_SIZE];
+
+
+  // definitions for the CSA adders
+  typedef logic_vector [1:0] two_words;
+  typedef logic_vector [3:0] four_words;
+  typedef logic_vector [5:0] six_words;
+  typedef logic_vector [7:0] eight_words;
+  typedef logic_vector [9:0] ten_words;
+
+endpackage
