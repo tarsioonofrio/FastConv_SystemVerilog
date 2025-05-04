64a88319b493f319b555161dfd61927e000395a0
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-05-04 01:21:24 -0300

    up

--- src/ifn9-06m20b/rtl/fast_conv.sv
+++ src/ifn9-06m20b/rtl/fast_conv.sv
@@ -31,7 +31,8 @@ module conv
  #(
     parameter int QUANT = 8,
     parameter int NBITS = 20,
-    parameter int NMULT = 6
+    parameter int NMULT = 6,
+    parameter int SMULT = 6
   )
   (
     input  logic       clk, reset, start,
@@ -44,7 +45,7 @@ module conv
   timeunit 1ns;
   timeprecision 1ps;
 
-  localparam logic [5:0] addr [0:5][0:5] = '{
+  localparam logic [5:0] addr [0:SMULT-1][0:NMULT-1] = '{
     '{  0,  1,  2,  3,  4,  5 },
     '{  6,  7,  8,  9, 10, 11 },
     '{ 12, 13, 14, 15, 16, 17 },
@@ -52,6 +53,10 @@ module conv
     '{ 24, 25, 26, 27, 28, 29 },
     '{ 30, 31, 32, 33, 34, 35 }
   };
+
+  // MU[SMULT]
+  typedef enum {MU[6], WR_OUT, IDLE, WR_IFMAP, WR_MC} state_type;
+  state_type current_st, next_st;
 
   type_weight   registers;
   type_matrix_c prod_c0;
@@ -63,9 +68,6 @@ module conv
 
   logic[5:0] idx[0:NMULT-1];
 
-  // up to 16 states
-  typedef enum {MU[6], WR_OUT, IDLE, WR_IFMAP, WR_MC} state_type;
-  state_type current_st, next_st;
 
   //
   // Control FSM
