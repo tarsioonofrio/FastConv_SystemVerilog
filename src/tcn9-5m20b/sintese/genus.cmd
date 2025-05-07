d0c250ef05711fc28df007264465922522d60572
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-05-07 20:30:32 -0300

    up

--- src/tcn9-5m20b/sintese/genus.cmd
+++
@@ -1,9 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 22.17-s071_1, built Sep 12 2024 12:37:05
-
-# Date: Sun May 04 23:19:24 2025
-# Host: hydra.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.47.1.el8_10.x86_64) (28cores*112cpus*2physical cpus*AMD EPYC 7453 28-Core Processor 512KB)
-# OS:   Rocky Linux release 8.10 (Green Obsidian)
-
-source run_logical_synthesis.tcl
-pwd
-exit
