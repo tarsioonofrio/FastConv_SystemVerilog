c4ae35ea2bb929cd2a8146a970202d2f2689f2f2
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-03 10:48:57 -0300

    up

---
+++ src/tcn4-08m20b/power_eval.sh
@@ -0,0 +1,13 @@
+cd ./sintese/
+module load genus > /dev/null 2>&1
+genus -f run_logical_synthesis.tcl
+
+cd ../simSDF/
+module purge
+module load xcelium > /dev/null 2>&1
+xrun -f file_list.f -input xrun.tcl
+
+cd ../sintese/
+module purge  > /dev/null 2>&1
+module load ddi
+genus -f run_power.tcl
