879bf0677a01c4ff379b92482abaa44cb0178b3b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-08 10:35:09 -0300

    up

--- scripts/time.py
+++ scripts/time.py
@@ -13,7 +13,7 @@ def format_column(folder):
 
 
 # Define o padrão do caminho para encontrar todos os arquivos time.csv
-path = "../src/*/data/time.csv"
+path = "../rtl/conv/*/data/time.csv"
 
 # Lista todos os arquivos que correspondem ao padrão
 all_files = glob.glob(path)
@@ -22,7 +22,7 @@ all_files = glob.glob(path)
 df = pd.concat(map(pd.read_csv, glob.glob(path)), ignore_index=True)
 # Pivota a tabela para o formato desejado
 df_pivot = df.pivot(index="side", columns="project", values="time/ns")
-df_pivot
+# df_pivot
 # df_pivot.drop(columns='project', inplace=True)
 # df_pivot
 # # Reordena as colunas caso necessário
