eabaaba707e78d372172d4cdb292c88d70eceb50
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-08 10:23:08 -0300

    up

--- src/ifn9-04m20b/simSDF/file_list.f
+++ src/ifn9-04m20b/simSDF/file_list.f
@@ -1,4 +1,4 @@
--smartorder -work work -top tb -gui -access +rw -maxdelays -sdf_cmd_file sdf_cmd.cmd
+-smartorder -work work -top tb -access +rw -maxdelays -sdf_cmd_file sdf_cmd.cmd
 /pdk/tsmc/PDK28/PDK_TSMC28_bv/tcbn28hpcplusbwp30p140_190a/TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp30p140_110a/tcbn28hpcplusbwp30p140.v
 ../sintese/results/gate_level/conv_logic_mapped.v
 ../data.sv
