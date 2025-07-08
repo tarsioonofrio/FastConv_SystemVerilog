e5efbcb7bae0476b5c19e1928fe9d5e811b907cc
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-08 17:17:38 -0300

    up

--- scripts/multiple-power-eval.sh
+++ scripts/multiple-power-eval.sh
@@ -6,20 +6,15 @@ for dir in "$(realpath "$1")"/*; do
   echo "****************************************************************************"
   cd "$dir" || { echo "Não foi possível entrar em $dir"; continue; }
 
-  if [ ! -f run_logical_synthesis.tcl ]; then
-    echo "Arquivo run_logical_synthesis.tcl não encontrado em $dir, pulando."
-    continue
-  fi
-
   cd ./sintese/
   module load genus > /dev/null 2>&1
   genus -f run_logical_synthesis.tcl
-  
+
   cd ../simSDF/
   module purge
   module load xcelium > /dev/null 2>&1
   xrun -f file_list.f -run -exit
-  
+
   cd ../sintese/
   module purge  > /dev/null 2>&1
   module load ddi
