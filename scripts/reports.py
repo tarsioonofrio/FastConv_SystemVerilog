e2a873e3c3e8e79692158bb39d9ffafcfdb2d348
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-08 10:35:09 -0300

    up

--- scripts/reports.py
+++ scripts/reports.py
@@ -26,7 +26,7 @@ def project_name(path):
 # Define o padrão do caminho para encontrar todos os arquivos rpt
 
 # Area
-path = Path("../src/*/sintese/results/reports/*_area.rpt")
+path = Path("../rtl/conv/*/sintese/results/reports/*_area.rpt")
 all_files = glob.glob(path.as_posix())
 report_area = {project_name(f): read_file(f) for f in all_files}
 cell_cout = {k: v[14].split()[1] for k, v in report_area.items()}
@@ -34,7 +34,7 @@ cell_area = {k: v[14].split()[2] for k, v in report_area.items()}
 net_area = {k: v[14].split()[3] for k, v in report_area.items()}
 total_area = {k: v[14].split()[4] for k, v in report_area.items()}
 
-path = Path("../src/*/sintese/results/reports/*_clock_gating.rpt")
+path = Path("../rtl/conv/*/sintese/results/reports/*_clock_gating.rpt")
 # clock_gating
 all_files = glob.glob(path.as_posix())
 report_clock = {project_name(f): read_file(f) for f in all_files}
@@ -42,7 +42,7 @@ flop_count = {k: v[-4].split()[1] for k, v in report_clock.items()}
 
 # Power
 path = Path(
-    "../src/*/sintese/results/reports/*_power_analysis_view_0p90v_25c_captyp_nominal.rpt"
+    "../rtl/conv/*/sintese/results/reports/*_power_analysis_view_0p90v_25c_captyp_nominal.rpt"
 )
 # clock_gating
 all_files = glob.glob(path.as_posix())
