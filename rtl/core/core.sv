76f38334bbac3be889be17a2ec0d2219438487da
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-07-16 21:46:42 -0300

    up

--- rtl/core/core.sv
+++ rtl/core/core.sv
@@ -54,7 +54,7 @@ module Core
 
   logic out_ce;
   logic out_we;
-  logic s_end;
+  logic r_data_end;
   logic output_valid;
   logic s_debug;
 
@@ -84,7 +84,7 @@ module Core
 
 
   logic signed [NBITS-1+QUANT:0] product;  // QUANT more bits for the multipliers
-  logic                          data_valid;
+  logic                          r_conv_end;
   logic        [            5:0] idx;
 
 
@@ -103,19 +103,18 @@ module Core
   always_comb begin
     unique case (current_st)
       IDLE:
-      if (p_wh_ce) next_st = WEIGHT;
-      else if (p_in_ce) next_st = DATA_IN;
-      else if (p_start) next_st = CONV;
-      WEIGHT: if (s_end) next_st = IDLE;
-      DATA_IN: if (s_end) next_st = IDLE;
-      CONV: if (output_valid) next_st = DATA_OUT;
-      DATA_OUT: if (serial_data_out) next_st = IDLE;
-      default: next_st = IDLE;
+        if (p_wh_ce) next_st = WEIGHT;
+        else if (p_in_ce) next_st = DATA_IN;
+        else if (p_start) next_st = CONV;
+      WEIGHT:   if (r_data_end) next_st = IDLE;
+      DATA_IN:  if (r_data_end) next_st = IDLE;
+      CONV:     if (current_st_conv) next_st = DATA_OUT;
+      DATA_OUT: if (r_data_end) next_st = IDLE;
     endcase
   end
 
   always_comb begin
-    p_end = s_end;
+    p_end = r_data_end;
     start_conv = p_start;
   end
 
@@ -124,58 +123,67 @@ module Core
     if (reset) begin
       registers_out <= '{default: '0};
       register_weight <= '{default: '0};
-      s_end <= 1'b0;
+      r_data_end <= 1'b0;
       count_to_serial <= 0;
       count_to_parallel <= 0;
       registers <= '{default: '0};
-      data_valid <= 0;
+      r_conv_end <= 0;
       idx <= 0;
     end else begin
       s_debug <= 1'b0;
-      if (output_valid) begin
-        registers_out <= output_map;
-        s_end <= 1'b1;
-      end
+      // if (output_valid) begin
+      //   registers_out <= output_map;
+      //   r_data_end <= 1'b1;
+      // end
       unique case (current_st)
         IDLE: begin
           registers_out <= '{default: '0};
-          s_end <= 1'b0;
+          r_data_end <= 1'b0;
           idx <= 0;
+          r_conv_end <= 0;
         end
         WEIGHT: begin
-          if (p_wh_ce && (count_to_parallel < 36)) begin
+          if (p_wh_ce && (count_to_parallel < M1_SIZE * M2_SIZE)) begin
             register_weight[count_to_parallel] <= serial_data_in;
             count_to_parallel <= count_to_parallel + 1;
-            s_end <= 1'b0;
+            r_data_end <= 1'b0;
           end else begin
             count_to_parallel <= 0;
-            s_end <= 1'b1;
+            r_data_end <= 1'b1;
           end
         end
         DATA_IN: begin
-          if (p_in_ce && (count_to_parallel < 25)) begin
+          if (p_in_ce && (count_to_parallel < C1_SIZE * C2_SIZE)) begin
             registers[count_to_parallel] <= serial_data_in;
             count_to_parallel <= count_to_parallel + 1;
-            s_end <= 1'b0;
+            r_data_end <= 1'b0;
           end else begin
             count_to_parallel <= 0;
-            s_end <= 1'b1;
+            r_data_end <= 1'b1;
           end
         end
         CONV: begin
-          data_valid <= 0;
           unique case (current_st_conv)
             WR_MC: registers <= prod_c;
-            WR_OUT: begin
-              data_valid <= 1;
-              registers[8:0] <= prod_a;
-            end
             MU: begin
               registers[idx] <= product;
               idx <= idx + 1;
             end
-            // default: registers[idx] <= product;
+            WR_OUT: begin
+              registers_out <= prod_a;
+              r_conv_end <= 1;
+            end
           endcase
+        end
+        DATA_OUT: begin
+          if (count_to_serial < M1_SIZE * M2_SIZE) begin
+            count_to_serial  <= count_to_serial + 1;
+            serial_valid_out <= 1'b1;
+          end else begin
+            count_to_serial  <= 0;
+            serial_valid_out <= 1'b0;
+            r_data_end <= 1'b1;
+          end
         end
       endcase
     end
@@ -185,7 +193,7 @@ module Core
 
   always_comb begin
     serial_data_in = p_in_data;
-    if (current_st == DATA_IN) input_map = parallel_data_out[24:0];
+    if (current_st == DATA_IN) input_map = parallel_data_out[C1_SIZE*C2_SIZE-1:0];
   end
 
 
@@ -193,7 +201,7 @@ module Core
   // BLOCK: serialize
 
   always_comb begin
-    serial_data_out = parallel_data_in[count_to_serial];
+    serial_data_out = registers_out[count_to_serial];
     parallel_data_out[count_to_parallel] = serial_data_in;
   end
 
@@ -208,7 +216,7 @@ module Core
     // inputMAP = input_map;
     weights = register_weight;
     output_map = outputMAP;
-    output_valid = data_valid;
+    output_valid = r_conv_end;
   end
 
   //
@@ -228,7 +236,7 @@ module Core
       IDLE1: next_st_conv = start ? WR_MC : IDLE1;
       WR_MC: next_st_conv = MU;
       MU:
-      if (idx < M1_SIZE*M2_SIZE-1) next_st_conv = MU;
+      if (idx < M1_SIZE * M2_SIZE - 1) next_st_conv = MU;
       else next_st_conv = WR_OUT;
       WR_OUT: next_st_conv = IDLE1;
     endcase
