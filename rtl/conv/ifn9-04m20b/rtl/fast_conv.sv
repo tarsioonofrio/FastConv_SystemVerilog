9174ec7acf6458fa721bb67d864a4aad69d6bb8b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-09-03 10:35:57 -0300

    padronizando todos os arquivos de fast convolution

--- rtl/conv/ifn9-04m20b/rtl/fast_conv.sv
+++ rtl/conv/ifn9-04m20b/rtl/fast_conv.sv
@@ -5,9 +5,10 @@
 //-------------------------------------------------------------------------
 // FAST CONVOLUTION
 //-------------------------------------------------------------------------
+import packConv::*;
+import data::*;
 
 module Multip
-  import packConv::*;
  #(
   parameter int QUANT = 8,
   parameter int NBITS = 20
@@ -30,9 +31,9 @@ module conv
   import packConv::*;
  #(
     parameter int QUANT = 8,
-    parameter int NBITS = 20,
-    parameter int NMULT = 4,
-    parameter int SMULT = 9
+    parameter int NBITS = 20
+    // parameter int NMULT = 6,
+    // parameter int SMULT = 6
   )
   (
     input  logic       clk, reset, start,
@@ -86,7 +87,7 @@ module conv
 
   // Instance of matrix multiplier "C"
   Transform trf(
-    .pin(registers[24:0]),
+    .pin(registers[C1_SIZE*C1_SIZE-1:0]),
     .pout(prod_c)
   );
 
@@ -113,11 +114,11 @@ module conv
       data_valid <= 0;
       unique case (current_st)
         IDLE:     registers <= registers;
-        WR_IFMAP: registers[24:0] <= inputMAP;
+        WR_IFMAP: registers[C1_SIZE*C1_SIZE-1:0] <= inputMAP;
         WR_MC:    registers <= prod_c;
         WR_OUT: begin
           data_valid <= 1;
-          registers[8:0] <= prod_a;
+          registers[A1_SIZE*A1_SIZE-1:0] <= prod_a;
         end
         default:  begin
           for (int i = 0; i < NMULT; i++) begin
@@ -130,7 +131,7 @@ module conv
 
   // connect 9 first registers to the outputs
   always_comb begin
-    outputMAP = registers[8:0];
+    outputMAP = registers[A1_SIZE*A1_SIZE-1:0];
   end
 
 endmodule
--- rtl/conv/tcn4-16m20b/rtl/fast_conv.sv
+++ rtl/conv/tcn4-16m20b/rtl/fast_conv.sv
@@ -1,5 +1,14 @@
+//-------------------------------------------------------------------------
+// FERNANDO MORAES                                          24/October/2024
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+// FAST CONVOLUTION
+//-------------------------------------------------------------------------
+import packConv::*;
+import data::*;
+
 module Multip
-  import packConv::*;
  #(
   parameter int QUANT = 8,
   parameter int NBITS = 20
@@ -21,14 +30,15 @@ endmodule
 module conv
   import packConv::*;
  #(
-   parameter int QUANT = 8,
-   parameter int NBITS = 20,
-   parameter int NMULT = 16
-   )
+    parameter int QUANT = 8,
+    parameter int NBITS = 20
+    // parameter int NMULT = 6,
+    // parameter int SMULT = 6
+  )
   (
     input  logic       clk, reset, start,
     input  type_input  inputMAP,
-    input  type_input  weights,
+    input  type_weight weights,
     output type_output outputMAP,
     output logic       data_valid
  );
@@ -36,17 +46,22 @@ module conv
   timeunit 1ns;
   timeprecision 1ps;
 
-  type_input    registers;
-  type_matrix_c prod_c;
+  state_type current_st, next_st;
+
+  type_weight   registers;
+  type_matrix_c prod_c0;
+  type_weight   prod_c;
   type_output   prod_a;
 
-  logic signed [NBITS-1+QUANT:0] product[0:NMULT];   // QUANT more bits for the multipliers
+  logic signed[NBITS-1+QUANT:0] product[0:NMULT-1];   // QUANT more bits for the multipliers
 
-  state_type current_st, next_st;
+  logic[5:0] idx[0:NMULT-1];
+
 
   //
   // Control FSM
   //
+
   always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
       current_st <= IDLE;
@@ -55,13 +70,14 @@ module conv
     end
   end
 
+  // 9 states + IDEL - IDLE is blocking!
   always_comb begin
     unique case (current_st)
       IDLE:     next_st = start ? WR_IFMAP : IDLE;
       WR_IFMAP: next_st = WR_MC;
-      WR_MC:    next_st = MU;
-      MU:       next_st = WR_OUT;
-      default:  next_st = IDLE;
+      WR_MC:    next_st = MU0;
+      WR_OUT:   next_st = IDLE;
+      default: next_st = state_type'(current_st + 1);
     endcase
   end
 
@@ -71,15 +87,15 @@ module conv
 
   // Instance of matrix multiplier "C"
   Transform trf(
-    .pin(registers),
+    .pin(registers[C1_SIZE*C1_SIZE-1:0]),
     .pout(prod_c)
   );
 
-  // assign idx = addr[current_st];
+  assign idx = addr[current_st];
 
   generate
     for (genvar i = 0; i < NMULT; i++) begin
-      Multip multip(.register(registers[i]), .weight(weights[i]), .product(product[i]));
+      Multip multip(.register(registers[idx[i]]), .weight(weights[idx[i]]), .product(product[i]));
     end
   endgenerate
 
@@ -95,26 +111,27 @@ module conv
       registers <= '{default: '0};
       data_valid <= 0;
     end else begin
-      data_valid <= 0;  // default
+      data_valid <= 0;
       unique case (current_st)
         IDLE:     registers <= registers;
-        WR_IFMAP: registers <= inputMAP;
+        WR_IFMAP: registers[C1_SIZE*C1_SIZE-1:0] <= inputMAP;
         WR_MC:    registers <= prod_c;
         WR_OUT: begin
           data_valid <= 1;
-          registers[3:0] <= prod_a;
+          registers[A1_SIZE*A1_SIZE-1:0] <= prod_a;
         end
         default:  begin
           for (int i = 0; i < NMULT; i++) begin
-            registers[i] <= product[i];
+            registers[idx[i]] <= product[i];
           end
         end
       endcase
     end
   end
 
+  // connect 9 first registers to the outputs
   always_comb begin
-    outputMAP = registers[3:0];
+    outputMAP = registers[A1_SIZE*A1_SIZE-1:0];
   end
 
 endmodule
