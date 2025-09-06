ae5d2f719b52ae614b4c302024464070312aa2ac
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-09-06 16:25:29 -0300

    Add synthesis and power scripts for fast-conv-mux simulation

---
+++ rtl/conv/fast-conv-mux/sim/ifn9-06m20b/run_logical_synthesis.tcl
@@ -0,0 +1,18 @@
+###############################################################################
+# TOP
+###############################################################################
+set TOP_MODULE conv
+
+set OUT_FILES ./results
+
+set GIT_ROOT [exec git rev-parse --show-toplevel]
+
+set HDL_FILES "
+   	${GIT_ROOT}/data/ifn9/data.sv
+   	../pack_conv.sv
+   	${GIT_ROOT}/rtl/csa/csa_lib.sv
+   	${GIT_ROOT}/rtl/mult-matrices/ifn9/mult_matrices.sv
+   	${GIT_ROOT}/rtl/conv/fast-conv-one/rtl/fast_conv.sv
+"
+
+source ${GIT_ROOT}/synthesis/run_logical_synthesis.tcl
