ce0d795dbb5d19b4f816dc28776af6d1efbf6f7f
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-09-08 00:51:16 -0300

    Add timeunit and timeprecision to mux_mult modules

--- rtl/conv/fast-conv-mux/sim/ifn9-06m20b/simSDF/file_list.f
+++ rtl/conv/fast-conv-mux/sim/ifn9-06m20b/simSDF/file_list.f
@@ -1,6 +1,3 @@
 -smartorder -work work -top tb -access +rw -maxdelays -sdf_cmd_file sdf_cmd.cmd
 /pdk/tsmc/PDK28/PDK_TSMC28_bv/tcbn28hpcplusbwp30p140_190a/TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp30p140_110a/tcbn28hpcplusbwp30p140.v
 ../sintese/results/gate_level/conv_logic_mapped.v
-../data.sv
-../rtl/pack_conv.sv
-../tb_conv.sv
