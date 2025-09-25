4a079ddc4092f27fa1e0283140d4db3bcccc73c7
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-09-25 00:49:41 -0300

    Update power analysis to use System module and add run scripts

--- synthesis/ifn9-06m/synth/run_power.tcl
+++ synthesis/ifn9-06m/synth/run_power.tcl
@@ -1,5 +1,5 @@
-set DB_FILE [file normalize [file join [pwd] "results/gate_level/conv_logic_mapped.db"]]
-set SHM [file normalize [file join [pwd] "../simSDF/conv.shm"]]
+set DB_FILE [file normalize [file join [pwd] "results/gate_level/system_logic_mapped.db"]]
+set SHM [file normalize [file join [pwd] "../simSDF/dut.shm"]]
 set GIT_ROOT [exec git rev-parse --show-toplevel]
 
 source ${GIT_ROOT}/synthesis/source/run_power.tcl
