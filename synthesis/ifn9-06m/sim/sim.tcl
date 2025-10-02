a7379479c81c0d35a7ca27dc3c0cd1d1221a0a96
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-10-02 14:22:13 -0300

    Refactor control and multiplier modules for clarity and scalability
    - Replace r_addr_bias from variable width to fixed 3-bit width - Expand
    and instantiate clock gating modules from RC_CG_MOD_5 to RC_CG_MOD_70+ -
    Redesign Control module with updated signals and improved clock gating -
    Implement multiple Multip, Multip_265 to Multip_269 modules with
    detailed netlist optimizations - Improve signal naming and hierarchy for
    better maintainability

--- synthesis/ifn9-06m/sim/sim.tcl
+++ synthesis/ifn9-06m/sim/sim.tcl
@@ -47,6 +47,7 @@ vlog -work work $define_flags -svinputport=relaxed ${GIT_ROOT}/rtl/system/testbe
 vsim -voptargs=+acc -t ns work.tb
 set StdArithNoWarnings 1
 set StdVitalGlitchNoWarnings 1
+do wave.do
 
 # all blocks
 #run 50000ns
