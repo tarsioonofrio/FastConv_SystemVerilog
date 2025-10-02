8aea08b64fda1cef3f81c6da54629988945bfd85
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-10-02 12:54:05 -0300

    Rename memory files and add explicit memory interface signals to System module and testbench

--- rtl/system/testbench.sv
+++ rtl/system/testbench.sv
@@ -26,6 +26,19 @@ module tb;
   logic p_start;
   logic p_end;
 
+  logic w_read_en;
+  logic w_read_wr;
+  logic w_read_valid;
+  logic[NADDR-1:0] w_read_addr;
+  logic_vector w_read_in;
+  logic_vector w_read_data;
+
+  logic w_write_chip;
+  logic w_write_en;
+  logic w_write_valid;
+  logic[NADDR-1:0] w_write_addr;
+  logic_vector w_write_data;
+  logic_vector w_write_out;
 
   int count_fout = 0;
 
@@ -51,7 +64,48 @@ module tb;
     .reset(reset),
 
     .p_start(p_start),
-    .p_end(p_end)
+    .p_end(p_end),
+
+    .p_read_en(w_read_en),
+    .p_read_addr(w_read_addr),
+    .p_read_valid(w_read_valid),
+    .p_read_data(w_read_data),
+
+    .p_write_en(w_write_en),
+    .p_write_addr(w_write_addr),
+    .p_write_data(w_write_data)
+  );
+
+  Memory #(
+    .NADDR(NADDR),
+    .NBITS(NBITS),
+    .LATENCY(LATENCY),
+    .ROM(ROM)
+  ) memory_read(
+    .clk(clk),
+    .reset(reset),
+    .chip_en(w_read_en),
+    .wr_en(w_read_wr),
+    .address(w_read_addr),
+    .data_in(w_read_in),
+    .data_out(w_read_data),
+    .data_valid(w_read_valid)
+  );
+
+  Memory #(
+    .NADDR(NADDR),
+    .NBITS(NBITS),
+    .LATENCY(LATENCY),
+    .ROM(0)
+  ) memory_write(
+    .clk(clk),
+    .reset(reset),
+    .chip_en(w_write_en),
+    .wr_en(w_write_en),
+    .address(w_write_addr),
+    .data_in(w_write_data),
+    .data_out(w_write_out),
+    .data_valid(w_write_valid)
   );
 
   // Inicialização dos sinais e reset
@@ -77,9 +131,9 @@ module tb;
       @(posedge clk);
       for (int j = 0; j < FOUT2_SIZE; j++) begin
         @(posedge clk);
-        wait(dut.control.p_write_en);
-        if ($signed(const_feat_out_batch[i][j]) != $signed(dut.control.p_write_data)) begin
-          $display("Time %0t | const_feat_out[%0d][%0d] = %0d | Output = %0d", $time, i, j, const_feat_out_batch[i][j], dut.control.p_write_data);
+        wait(w_write_en);
+        if ($signed(const_feat_out_batch[i][j]) != $signed(w_write_data)) begin
+          $display("Time %0t | const_feat_out[%0d][%0d] = %0d | Output = %0d", $time, i, j, const_feat_out_batch[i][j], w_write_data);
           $display("=== ERROR - End simulation ====");
         end
       end
