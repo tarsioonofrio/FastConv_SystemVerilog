a603975e4f925d263d132af55a938ce075ed4ae2
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-10-05 23:40:22 -0300

    Rename synthesis/ifn9-06m to synthesis/ifn9-06m/032 and update paths

--- synthesis/ifn9-06m/032/sim/run-sim.sh
+++ synthesis/ifn9-06m/032/sim/run-sim.sh
@@ -10,16 +10,16 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 # Testbench e pack conforme usado no histórico
 TB=${GIT_ROOT}/rtl/system/testbench-synth.sv
 
-GATE=../logical/results/gate_level/system_logic_mapped.v
+GATE=../../logical/results/gate_level/system_logic_mapped.v
 
 # Monta lista de arquivos (uma só linha, sem newline), prefixando GIT_ROOT
 files=""
 while IFS= read -r line; do
   files="$files$GIT_ROOT/$line "
-done < ../list-file.txt
+done < ../../list-file.txt
 
 # Monta defines: prefixa -define em cada linha e junta em uma só linha
 # defines=$(sed 's/^/-define /' list_def.txt | tr '\n' ' ' | sed 's/ $//')
 
 # Chamada do xrun (mantendo args.txt como no histórico)
-xrun -f args.txt $files $TB $GATE -f ../list-define.txt -run -exit
+xrun -f args.txt $files $TB $GATE -f ../../list-define.txt -run -exit
