e70dc0e37e10622379318a4dadc31a9d9a05d30d
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-10-08 01:44:12 -0300

    Rename synthesis scripts for consistency and update paths accordingly

--- synthesis/ifn9-06m/sim/sim.tcl
+++ synthesis/ifn9-06m/sim/sim.tcl
@@ -15,7 +15,7 @@ set GIT_ROOT [exec git rev-parse --show-toplevel]
 # vlog -work work  -svinputport=relaxed $DATA_SV
 
 # Read key=value defines from define.txt and build the +define+key=value flags
-set defines_file "../../list-def.txt"
+set defines_file "../list-def.txt"
 set define_flags ""
 
 if {[file exists $defines_file]} {
@@ -35,7 +35,7 @@ if {[file exists $defines_file]} {
 # Read the file_list.txt file and execute vlog commands for each line, passing defines
 # vlog -work work $define_flags -svinputport=relaxed $DATA_FILE
 
-set file_list "../../list-file.txt"
+set file_list "../list-file.txt"
 set fp [open $file_list r]
 while {[gets $fp line] >= 0} {
     if {[string trim $line] ne ""} {
@@ -45,7 +45,7 @@ while {[gets $fp line] >= 0} {
 close $fp
 
 vlog -work work $define_flags -svinputport=relaxed /pdk/tsmc/PDK28/PDK_TSMC28_bv/tcbn28hpcplusbwp30p140_190a/TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp30p140_110a/tcbn28hpcplusbwp30p140.v
-vlog -work work $define_flags -svinputport=relaxed ../../logical/results/gate_level/system_logic_mapped.v
+vlog -work work $define_flags -svinputport=relaxed ../logical/results/gate_level/system_logic_mapped.v
 vlog -work work $define_flags -svinputport=relaxed ${GIT_ROOT}/rtl/system/testbench-netlist.sv
 # to show FSM
 # vsim -voptargs=+acc -t ns -fsmdebug -coverage -debugDB work.tb
