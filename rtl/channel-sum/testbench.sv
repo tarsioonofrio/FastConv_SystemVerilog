a8e06f60090d15c2e144bec05299177d6b114cda
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-10-22 21:00:57 -0300

    Refactor channel-sum testbench signals and waveforms
    - Rename and reorganize signals for clarity in channel-sum testbench -
    Adjust memory interface signals for consistent naming - Update testbench
    connections to reflect new signal structure - Enhance waveform
    configuration to include relevant signals and groups

--- rtl/channel-sum/testbench.sv
+++ rtl/channel-sum/testbench.sv
@@ -16,18 +16,23 @@ module tb;
   logic p_conv_start;
   logic p_conv_idle;
   logic p_conv_end;
+  logic p_sum_end;
+  logic p_conv_end_control;
+  logic p_start_channel;
 
   type_input p_input;
   type_weight p_weight;
-  type_output p_output_sum;
   type_output p_output_conv;
+  type_output p_input_channel;
+  type_output p_output_channel;
+  type_output p_output_control;
 
   logic w_read_en;
   logic w_read_wr;
   logic w_read_valid;
   logic[NADDR-1:0] w_read_addr;
-  logic_vector w_read_in;
-  logic_vector w_read_data;
+  logic_vector w_read_data_in;
+  logic_vector w_read_data_out;
 
   logic w_write_chip;
   logic w_write_valid;
@@ -37,10 +42,10 @@ module tb;
   logic[NADDR-1:0] w_write_addr;
   logic[NADDR-1:0] w_write_addr_control;
   logic[NADDR-1:0] w_read_addr_channel;
-  logic_vector w_write_data;
+  logic_vector w_write_data_in;
   logic_vector w_write_data_control;
   logic_vector w_read_data_channel;
-  logic_vector w_write_out;
+  logic_vector w_write_data_out;
 
 
   int count_fout = 0;
@@ -67,14 +72,14 @@ module tb;
     .clk(clk),
     .reset(reset),
 
-    .p_start(p_conv_start),
-    .p_end(p_start),
-    .p_input(p_output_conv),
-    .p_output(p_output_sum),
+    .p_start(p_start_channel),
+    .p_end(p_end_channel),
+    .p_input(p_input_channel),
+    .p_output(p_output_channel),
 
     .p_read_en(w_read_en_channel),
     .p_read_addr(w_read_addr_channel),
-    .p_read_data(w_read_data_channel),
+    .p_read_data(w_write_data_out),
     .p_read_valid(w_write_valid)
 
     // .p_write_en(w_write_en),
@@ -101,19 +106,19 @@ module tb;
     .p_end(p_end),
     .p_conv_start(p_conv_start),
     .p_conv_idle(p_conv_idle),
-    .p_conv_end(p_conv_end),
+    .p_conv_end(p_conv_end_control),
     .p_input(p_input),
     .p_weight(p_weight),
-    .p_output(p_output_sum),
+    .p_output(p_output_control),
 
     .p_read_en(w_read_en),
     .p_read_addr(w_read_addr),
     .p_read_valid(w_read_valid),
-    .p_read_data(w_read_data),
+    .p_read_data(w_read_data_out),
 
     .p_write_en(w_write_en_control),
     .p_write_addr(w_write_addr_control),
-    .p_write_data(w_write_data_control)
+    .p_write_data(w_write_data_in)
   );
 
   Memory #(
@@ -127,8 +132,8 @@ module tb;
     .chip_en(w_read_en),
     .wr_en(w_read_wr),
     .address(w_read_addr),
-    .data_in(w_read_in),
-    .data_out(w_read_data),
+    .data_in(w_read_data_in),
+    .data_out(w_read_data_out),
     .data_valid(w_read_valid)
   );
 
@@ -143,8 +148,8 @@ module tb;
     .chip_en(w_write_en),
     .wr_en(w_write_en),
     .address(w_write_addr),
-    .data_in(w_write_data),
-    .data_out(w_write_out),
+    .data_in(w_write_data_in),
+    .data_out(w_write_data_out),
     .data_valid(w_write_valid)
   );
 
@@ -163,16 +168,22 @@ module tb;
     .p_output(p_output_conv)
   );
 
-  
+
   always_comb begin
-    if (w_write_en_control) begin
+    if (w_write_addr_control < FEAT_OUTPUT_SIZE * FEAT_OUTPUT_SIZE ) begin
       w_write_en = w_write_en_control;
       w_write_addr = w_write_addr_control;
-      w_write_data = w_write_data_control;
+      p_conv_end_control = p_conv_end;
+      p_output_control = p_output_conv;
     end else begin
       w_write_en = w_read_en_channel;
       w_write_addr = w_read_addr_channel;
-      w_write_data = w_read_data_channel;
+      
+      p_start_channel = p_conv_end;
+      p_conv_end_control = p_end_channel;
+   
+      p_input_channel = p_output_conv;
+      p_output_control = p_output_channel;
     end
   end
 
@@ -199,8 +210,8 @@ module tb;
       for (int j = 0; j < FOUT2_SIZE; j++) begin
         @(posedge clk);
         wait(w_write_en);
-        if ($signed(const_feat_out_batch[i][j]) != $signed(w_write_data)) begin
-          $display("Time %0t | const_feat_out[%0d][%0d] = %0d | Output = %0d", $time, i, j, const_feat_out_batch[i][j], w_write_data);
+        if ($signed(const_feat_out_batch[i][j]) != $signed(w_write_data_in)) begin
+          $display("Time %0t | const_feat_out[%0d][%0d] = %0d | Output = %0d", $time, i, j, const_feat_out_batch[i][j], w_write_data_in);
           $display("=== ERROR - End simulation ====");
         end
       end
