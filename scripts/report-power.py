4af1d48ffda975f76021a04261a0dcfc5867b629
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-20 01:34:56 -0300

    Update report-power.py

--- scripts/report-power.py
+++ scripts/report-power.py
@@ -42,8 +42,8 @@ list_df = {
     f: pd.DataFrame(columns=columns, index=indexes, data=list(d))
     for f, d in zip(file_name, data)
 }
-for f, df in list_df.items():
-    df.to_csv(f"../data/{f}_power_report.csv")
+# for f, df in list_df.items():
+#     df.to_csv(f"../report/{f}_power_report.csv")
 df_total = pd.DataFrame({f: df["Total"] for f, df in list_df.items()}).T
 df_total.index.name = "file"
 df_total.to_csv("../report/category_power_report.csv")
