516708d61020966a5523d144104b931135319380
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-20 23:37:55 -0300

    Update mux_mult_06_lut.sv

--- rtl/mux-mult/ifn9/mux_mult_06_lut.sv
+++ rtl/mux-mult/ifn9/mux_mult_06_lut.sv
@@ -2,7 +2,16 @@
 // Index multiplexer module for selecting register indices based on state
 //-------------------------------------------------------------------------
 
+package pack_mux_mult;
+  parameter int NMULT = 6;
+  parameter int SMULT = 6;
+endpackage
+
 module MuxMult
+  #(
+    parameter int NMULT = 6,
+    parameter int SMULT = 6
+  )
   (
     input  logic[$clog2(SMULT-1):0] idx_in, // current state
     output logic[$clog2(SMULT*NMULT-1):0] idx_out[0:NMULT-1]  // index array output
@@ -11,8 +20,6 @@ module MuxMult
   timeunit 1ns;
   timeprecision 1ps;
 
-  localparam int NMULT = 6;
-  localparam int SMULT = 6;
   localparam logic [5:0] addr [0:SMULT-1][0:NMULT-1] = '{
     '{  0,  1,  2,  3,  4,  5 },
     '{  6,  7,  8,  9, 10, 11 },
@@ -20,7 +27,7 @@ module MuxMult
     '{ 18, 19, 20, 21, 22, 23 },
     '{ 24, 25, 26, 27, 28, 29 },
     '{ 30, 31, 32, 33, 34, 35 }
-  };  
+  };
 
   assign idx_out = addr[idx_in];
 endmodule
