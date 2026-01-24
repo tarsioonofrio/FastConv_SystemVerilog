3c252539b12efdf883adf2f683818a201b98a747
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-23 22:41:58 -0300

    Update logical_synthesis.tcl

--- synthesis/_naive-conv/logical/logical_synthesis.tcl
+++ synthesis/_naive-conv/logical/logical_synthesis.tcl
@@ -10,6 +10,6 @@ set GIT_ROOT [exec git rev-parse --show-toplevel]
 
 set DEFINE_FLAGS ""
 
-set HDL_FILES "../rtl/mac_op9.sv ../rtl/naive_conv.sv ../rtl/pack_conv.sv"
+set HDL_FILES "../rtl/pack_conv.sv ../rtl/mac_op9.sv ../rtl/naive_conv.sv"
 
 source ${GIT_ROOT}/synthesis/_source/logical_synthesis.tcl
