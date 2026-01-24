2c5a4296fba4771d92900e62b3a583ccc3235bc9
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-23 22:36:49 -0300

    Add naive conv synthesis, sim and power scripts

---
+++ synthesis/_naive-conv/logical/logical_synthesis.tcl
@@ -0,0 +1,13 @@
+###############################################################################
+# TOP
+###############################################################################
+
+set TOP_MODULE conv
+
+set OUT_FILES "[pwd]/results"
+
+set GIT_ROOT [exec git rev-parse --show-toplevel]
+
+set HDL_FILES "../rtl/mac_op9.sv ../rtl/naive_conv.sv ../rtl/pack_conv.sv"
+
+source ${GIT_ROOT}/synthesis/_source/logical_synthesis.tcl
