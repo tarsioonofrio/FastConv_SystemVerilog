611e92a56230487c357e3442ab9c736af310eeee
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-03-30 21:23:02 -0300

    Create genus.cmd

---
+++ genus.cmd
@@ -0,0 +1,13 @@
+# Cadence Genus(TM) Synthesis Solution, Version 21.12-s068_1, built Mar  4 2022 14:12:46
+
+# Date: Mon Mar 30 21:07:08 2026
+# Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.83.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
+# OS:   Rocky Linux release 8.10 (Green Obsidian)
+
+help create_clock
+help create_clock
+create_clock --help
+create_clock --help
+clear
+create_clock -help
+create_clock help
