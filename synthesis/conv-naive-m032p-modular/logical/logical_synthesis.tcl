d425c8c11bf501f47597dfa4219d5711353e5efa
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-04-06 11:57:45 -0300

    add naive modularized with synth

---
+++ synthesis/conv-naive-m032p-modular/logical/logical_synthesis.tcl
@@ -0,0 +1,15 @@
+###############################################################################
+# TOP
+###############################################################################
+
+set TOP_MODULE conv
+
+set OUT_FILES "[pwd]/results"
+
+set GIT_ROOT [exec git rev-parse --show-toplevel]
+
+set DEFINE_FLAGS ""
+
+set HDL_FILES "../data.sv ../rtl/pack_conv.sv ../rtl/mac_op9.sv ../rtl/naive_conv.sv"
+
+source ${GIT_ROOT}/synthesis/_source/logical_synthesis.tcl
