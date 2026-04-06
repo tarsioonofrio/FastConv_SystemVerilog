2db0ea5e8f1e9ea9982ee7462f4a17d5682b5112
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-04-06 13:12:31 -0300

    Update genus.cmd

--- synthesis/conv-naive-m032p-modular/logical/genus.cmd
+++ synthesis/conv-naive-m032p-modular/logical/genus.cmd
@@ -1,7 +1,6 @@
 # Cadence Genus(TM) Synthesis Solution, Version 21.12-s068_1, built Mar  4 2022 14:12:46
 
-# Date: Mon Apr 06 11:42:47 2026
+# Date: Mon Apr 06 11:58:10 2026
 # Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.83.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
 # OS:   Rocky Linux release 8.10 (Green Obsidian)
 
-source logical_synthesis.tcl
