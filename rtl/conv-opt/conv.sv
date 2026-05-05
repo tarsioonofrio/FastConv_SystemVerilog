3756696cba70374395788312628ef0912be37bd2
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-05-05 19:24:17 -0300

    Extract Multip module to rtl/multip/multip.sv
    Rename instance ports in conv to register_input and weight_input and
    remove duplicated Multip implementations from conv and conv-opt. Add the
    new rtl/multip/multip.sv to the various list-file.txt files.

--- rtl/conv-opt/conv.sv
+++ rtl/conv-opt/conv.sv
@@ -157,19 +157,3 @@ module Conv
 endmodule
 
 
-module Multip
-  import pack_typedef::*;
-#(
-    parameter int QUANT = 8,
-    parameter int NBITS = 20
-) (
-    input logic_vector register_input,
-    input logic_vector weight_input,
-    output logic signed [NBITS-1+QUANT:0] product
-);
-  timeunit 1ns; timeprecision 1ps;
-  logic signed [NBITS-1+QUANT:0] partial_product;
-
-  assign partial_product = (NBITS + QUANT)'($signed(register_input) * $signed(weight_input));
-  assign product = (NBITS)'(partial_product[NBITS-1+QUANT:QUANT]);
-endmodule
