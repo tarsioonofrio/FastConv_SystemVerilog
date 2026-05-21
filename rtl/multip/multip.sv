4eba52c35a49ac67549ae84db16255eb06745861
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-05-21 20:28:18 -0300

    Rename output addressing and multiplier ports

--- rtl/multip/multip.sv
+++ rtl/multip/multip.sv
@@ -2,8 +2,8 @@ module Multip #(
     parameter int QUANT = 8,
     parameter int NBITS = 20
 ) (
-    input logic [NBITS-1:0] register_input,
-    input logic [NBITS-1:0] weight_input,
+    input logic [NBITS-1:0] feature,
+    input logic [NBITS-1:0] weight,
     output logic signed [NBITS-1+QUANT:0] product
 );
   timeunit 1ns;
@@ -11,6 +11,6 @@ module Multip #(
 
   logic signed [NBITS-1+QUANT:0] partial_product;
 
-  assign partial_product = (NBITS + QUANT)'($signed(register_input) * $signed(weight_input));
+  assign partial_product = (NBITS + QUANT)'($signed(feature) * $signed(weight));
   assign product = (NBITS)'(partial_product[NBITS-1+QUANT:QUANT]);
 endmodule
