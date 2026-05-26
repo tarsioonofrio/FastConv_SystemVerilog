0c44371efa951445a5017206417de601e69124a1
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-05-26 16:04:20 -0300

    Update sim.tcl

--- rtl/control/sim.tcl
+++ rtl/control/sim.tcl
@@ -10,6 +10,7 @@ set ROM 1
 # set DATA data/ifn9/sim/sim-032-1-1-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-1-3-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-2-1-seq/pack_data.sv
+# set DATA data/ifn9/sim/sim-032-3-1-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-2-2-seq/pack_data.sv
 set DATA data/ifn9/sim/sim-032-3-3-seq/pack_data.sv
 set PARAM rtl/pack-param/ifn9/pack_param.sv
