5dbff0604601d80f4f1ae99901137ef6958e799f
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-05-26 20:21:17 -0300

    Update sim.tcl

--- rtl/conv3x3/sim.tcl
+++ rtl/conv3x3/sim.tcl
@@ -45,7 +45,7 @@ set file_list [list \
 vlog -work work $define_flags -svinputport=relaxed {*}$file_list
 
 # vlog -work work +define+SIMULATION $define_flags -svinputport=relaxed ./control.sv
-vlog -work work $define_flags -svinputport=relaxed ./control.sv
+vlog -work work $define_flags -svinputport=relaxed ./conv.sv
 vlog -work work $define_flags -svinputport=relaxed ./testbench.sv
 # to show FSM
 # vsim -voptargs=+acc -t ps -fsmdebug -coverage -debugDB work.tb
