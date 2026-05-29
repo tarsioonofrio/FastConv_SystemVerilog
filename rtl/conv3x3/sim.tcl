c2aa7b119ae25e63ba49e4a4e98fc7aa1c0100a9
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-05-29 16:01:35 -0300

    Enable output write for final beat
    Update simulation files to use memory_write signals and adjust
    waveform settings. Change mem.do to add /tb/memory_write/data,
    add memory_write waves in wave.do, and update DATA selection in
    sim.tcl.

--- rtl/conv3x3/sim.tcl
+++ rtl/conv3x3/sim.tcl
@@ -7,12 +7,12 @@ set LATENCY 1
 set ROM 1
 # set QUANT 8
 
-# set DATA data/ifn9/sim/sim-032-1-1-seq/pack_data.sv
+set DATA data/ifn9/sim/sim-032-1-1-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-1-3-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-2-1-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-3-1-seq/pack_data.sv
 # set DATA data/ifn9/sim/sim-032-2-2-seq/pack_data.sv
-set DATA data/ifn9/sim/sim-032-3-3-seq/pack_data.sv
+# set DATA data/ifn9/sim/sim-032-3-3-seq/pack_data.sv
 set PARAM pack-param/ifn9/pack_param.sv
 set MUX mux-mult/ifn9/mux_mult_06.sv
 set MULT mult-matrices/ifn9/mult_matrices_csa.sv
