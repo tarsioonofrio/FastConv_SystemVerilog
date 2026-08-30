0e3feeac0c4b0f1a3cf335b1c36af82aac4ab4fa
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-08-30 18:17:23 -0300

    refactor(synthesis): localize flows per convolution

--- synthesis/_template_sys/power/power.tcl
+++ synthesis/_template_sys/power/power.tcl
@@ -1,5 +1,24 @@
-set DB_FILE [file normalize [file join [pwd] "../logical/results/gate_level/system_logic_mapped.db"]]
-set SHM [file normalize [file join [pwd] "../sim/dut.shm"]]
-set GIT_ROOT [exec git rev-parse --show-toplevel]
+###############################################################################
+# Project-local power analysis flow.
+###############################################################################
 
-source ${GIT_ROOT}/synthesis/_source/power.tcl
+set POWER_ROOT [file normalize [file dirname [info script]]]
+set CONFIG_ROOT [file normalize [file join $POWER_ROOT ..]]
+set GIT_ROOT [exec git -C $CONFIG_ROOT rev-parse --show-toplevel]
+set TOP_MODULE system
+set top_file [file join $CONFIG_ROOT top-module.txt]
+if {[file exists $top_file]} {
+    set fp_top [open $top_file r]
+    while {[gets $fp_top line] >= 0} {
+        set line_trim [string trim $line]
+        if {$line_trim ne "" && ![string match "#*" $line_trim]} {
+            set TOP_MODULE $line_trim
+            break
+        }
+    }
+    close $fp_top
+}
+set DB_FILE [file normalize [file join $CONFIG_ROOT logical results gate_level ${TOP_MODULE}_logic_mapped.db]]
+set SHM [file normalize [file join $CONFIG_ROOT sim dut.shm]]
+
+source [file join $CONFIG_ROOT scripts power.tcl]
