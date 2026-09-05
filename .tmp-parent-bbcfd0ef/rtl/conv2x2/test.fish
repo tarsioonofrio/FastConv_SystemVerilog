e4bdc8b4fa9470b5b902b73d142a8c56b4cff6d0
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-09-05 10:35:34 -0300

    removing temp folder

--- .tmp-parent-bbcfd0ef/rtl/conv2x2/test.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-modelsim-set-path; or exit $status
-set -l SCRIPT_DIR (dirname (status -f))
-pushd "$SCRIPT_DIR" >/dev/null
-vsim -c -do sim.tcl
-set -l sim_status $status
-popd >/dev/null
-exit $sim_status
