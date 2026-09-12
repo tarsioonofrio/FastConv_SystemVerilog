b4a619f90999a430c5c1371df534a6814cb59367
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-09-12 10:47:20 -0300

    power: complete missing Paxos flows

--- rtl/conv2x2/synthesis/conv-i16-h16-t16-o4-m08-std/power/genus.cmd
+++ rtl/conv2x2/synthesis/conv-i16-h16-t16-o4-m08-std/power/genus.cmd
@@ -1,7 +1,7 @@
-# Cadence Genus(TM) Synthesis Solution, Version 23.14-s090_1, built Feb 27 2025 10:49:50
+# Cadence Genus(TM) Synthesis Solution, Version 21.12-s068_1, built Mar  4 2022 14:12:46
 
-# Date: Sun Aug 30 19:20:42 2026
+# Date: Sat Sep 12 09:43:47 2026
 # Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.155.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
-# OS:   Rocky Linux 8.10 (Green Obsidian)
+# OS:   Rocky Linux release 8.10 (Green Obsidian)
 
 source power.tcl
