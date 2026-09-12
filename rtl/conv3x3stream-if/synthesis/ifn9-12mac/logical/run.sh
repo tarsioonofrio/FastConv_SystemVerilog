8ca92ef8c48d488300be8633b55f499b60152c2a
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-09-12 09:12:46 -0300

    fix(synthesis): initialize Cadence modules in flow scripts

--- rtl/conv3x3stream-if/synthesis/ifn9-12mac/logical/run.sh
+++ rtl/conv3x3stream-if/synthesis/ifn9-12mac/logical/run.sh
@@ -1,6 +1,8 @@
 rm -rf genus.cmd*
 rm -rf genus.log*
 
+source /usr/share/Modules/init/bash
 module purge
-module load genus > /dev/null 2>&1
+module use /soft64/modulefiles
+module load cadence/genus/211 > /dev/null 2>&1
 genus -f logical_synthesis.tcl
